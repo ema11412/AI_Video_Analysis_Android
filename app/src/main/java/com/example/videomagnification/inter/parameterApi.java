7e2061108d206918dbe80ac3365571089418717c
Emanuel Esquivel <ema11412@hotmail.com>
2022-04-06 16:14:43 -0600

    [ADD][Interface]

--- app/src/main/java/com/example/videomagnification/inter/parameterApi.java
+++ app/src/main/java/com/example/videomagnification/inter/parameterApi.java
@@ -5,11 +5,18 @@ import com.example.videomagnification.model.Params;
 
 import java.util.List;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface parameterApi {
 
-    @GET("predict/parameter/{image}")
-    Call<List<Params>> getParams(@Path("image") String image);
+    @POST("predict/parameter")
+    Call<Params> getParams(
+            @Query("base64_image") String base64_image
+    );
+
+
 }
--- app/src/main/java/com/example/videomagnification/processing/magnificators/MagnificatorFactory.java
+++ app/src/main/java/com/example/videomagnification/processing/magnificators/MagnificatorFactory.java
@@ -1,12 +1,14 @@
 package com.example.videomagnification.processing.magnificators;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 
 import com.example.videomagnification.R;
 import com.example.videomagnification.application.App;
 import com.example.videomagnification.inter.parameterApi;
 import com.example.videomagnification.model.Params;
 
+import java.io.IOException;
 import java.util.List;
 
 import retrofit2.Call;
@@ -107,38 +109,86 @@ public class MagnificatorFactory {
 
         return null;
     }
-    private void getParams(){
 
-        int alp0;
-        float lamb0;
+    // new Peticion().execute();
+    public static class NetworkClient {
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://127.0.0.1:8000/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+        public static final String BASE_URL="http://192.168.0.114:5000/";
 
-        parameterApi parameterapi = retrofit.create(parameterApi.class);
+        public static Retrofit retrofit;
 
-        Call<List<Params>> call = parameterapi.getParams("3");
+   /*
+    This public static method will return Retrofit client
+    anywhere in the appplication
+    */
 
-        call.enqueue(new Callback<List<Params>>() {
+        public static Retrofit getRetrofitClient(){
+
+            //If condition to ensure we don't create multiple retrofit instances in a single application
+            if (retrofit==null) {
+
+                //Defining the Retrofit using Builder
+                retrofit=new Retrofit.Builder()
+                        .baseUrl(BASE_URL)   //This is the only mandatory call on Builder object.
+                        .addConverterFactory(GsonConverterFactory.create()) // Convertor library used to convert response into POJO
+                        .build();
+            }
+
+            return retrofit;
+        }
+
+    }
+
+
+    private void getParameters(String image) {
+
+        //Obtain an instance of Retrofit by calling the static method.
+        Retrofit  retrofit= NetworkClient.getRetrofitClient();
+
+       /*
+       The main purpose of Retrofit is to create HTTP calls from the Java interface based
+       on the annotation associated with each method. This is achieved by just passing the interface
+       class as parameter to the create method
+       */
+        parameterApi weatherAPIs = retrofit.create(parameterApi.class);
+
+       /*
+       Invoke the method corresponding to the HTTP request which will return a Call object. This Call
+       object will used to send the actual network request with the specified parameters
+       */
+
+
+        Call call = weatherAPIs.getParams(image);
+
+       /*
+        This is the line which actually sends a network request. Calling enqueue() executes a call asynchronously
+        It has two callback listeners which will invoked on the main thread
+        */
+        call.enqueue(new Callback() {
             @Override
-            public void onResponse(Call<List<Params>> call, Response<List<Params>> response) {
+            public void onResponse(Call call, Response response) {
+              /*This is the success callback. Though the response type is JSON, with Retrofit we get
+              the response in the form of WResponse POJO class
+              */
+                if (response.body()!=null) {
+                    Params params = (Params) response.body();
 
-                List<Params> paramsList = response.body();
-                for (Params param: paramsList){
-                    int alp0 = param.getAlp0();
-                    float lamb0 = param.getLamb0();
+                    int alp0 = params.getAlp0();
+                    int lamb0 = params.getLamb0();
                 }
             }
 
             @Override
-            public void onFailure(Call<List<Params>> call, Throwable t) {
+            public void onFailure(Call call, Throwable t) {
+              /*
+              Error callback
+              */
 
             }
-
         });
 
     }
 
+
 }
+
