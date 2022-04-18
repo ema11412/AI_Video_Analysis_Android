ca779c6322bc7ad16fb5a10d1b629c391686cf4b
Emanuel Esquivel López <ema11412@hotmail.com>
2022-04-18 15:26:09 -0700

    [UPDATE][Interface]

--- app/src/main/java/com/example/videomagnification/inter/ParameterApi.java
+++ app/src/main/java/com/example/videomagnification/inter/ParameterApi.java
@@ -3,15 +3,13 @@ package com.example.videomagnification.inter;
 import com.example.videomagnification.model.Params;
 
 
-import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 
-public interface parameterApi {
+
+public interface ParameterApi {
 
     @GET("predict/parameter/")
     Call<Params> getParams(@Query("base64_image") String base64_image);
